4bfe8ecca822e48b4241d5fab7fb5fd3357578b7
theval-s <Valchara3103@gmail.com>
2023-03-29 12:30:37 +0300

    lab4 task

--- ITMO-University/AlgosAndDataStructs/Lab4/Source.cpp
+++ ITMO-University/AlgosAndDataStructs/Lab4/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 void bubble_sort(vector<int>& vec) {
@@ -17,8 +18,32 @@ void bubble_sort(vector<int>& vec) {
 }
 
 int main() {
+	/*
 	vector<int> test = { 5, 27, 1, 18, 54 };
 	bubble_sort(test);
-	for (auto i : test) cout << i << " ";
+	for (auto i : test) cout << i << " ";*/
+
+	int n, m;
+	cin >> n >> m;
+	vector<pair<int,int>> segm(n);
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		segm[i] = make_pair(a, b);
+	}
+	sort(segm.begin(), segm.end());
+	//sort from <algorithm> - nlogn complexity
+	//pair are sorted for both first and second values
+		for (int i = 0; i < m; i++) {
+		int p;
+		cin >> p;
+		int cnt = 0;
+		for (int j = 0; j < n; j++) {
+			if (p < segm[j].first) break;
+			else if (p > segm[j].second) continue;
+			else cnt++;
+		}
+		cout << cnt << " ";
+	}
 	return 0;
 }
